16b3a4e70b3f2c9b1699ccd452719d4bebae89c9
FLSoz <hades5689@gmail.com>
2020-06-10 23:13:05 -0400

    Initial Upload

---
+++ IngressPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using Harmony;
+
+namespace EnemiesTargetLeadPatch
+{
+    public static class IngressPoint
+    {
+        public static void Main()
+        {
+            HarmonyInstance.Create("flsoz.ttmm.enemytargetlead.mod").PatchAll(Assembly.GetExecutingAssembly());
+
+            try
+            {
+                WeaponAimMod.src.WrappedDataHolder.cheatDisabled = true;
+            }
+            catch
+            {
+                Console.WriteLine("Failed to enable enemy target lead");
+            }
+        }
+    }
+}
